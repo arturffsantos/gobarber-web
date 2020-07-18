3d47cd7c4e858161d3673b1ffb74e5feb0376a92
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-07-17 22:19:50 -0300

    login page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobaStyle from './styles/global';
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <h1>GoBarber Web</h1>
+    <SignIn />
     <GlobaStyle />
   </>
 );
